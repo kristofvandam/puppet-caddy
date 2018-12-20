2680e50545b8282d13f13ac9f13dca6f7c0d801a
Kristof Vandam <kristof.vandam@gmail.com>
2018-12-20 13:49:25 +0100

    systemd fixes

--- manifests/config.pp
+++ manifests/config.pp
@@ -61,14 +61,8 @@ class caddy::config inherits caddy {
         owner   => 'root',
         group   => 'root',
         content => template('caddy/etc/systemd/system/caddy.service.erb'),
-        notify  => Exec['systemctl-daemon-reload'],
+        notify  => Class['caddy::service'],
         require => Class['caddy::package'],
-      }
-
-      exec {'systemctl-daemon-reload':
-        refreshonly => true,
-        path        => '/usr/bin:/usr/sbin:/bin:/usr/local/bin',
-        command     => 'systemctl daemon-reload',
       }
     }
     '6': {
