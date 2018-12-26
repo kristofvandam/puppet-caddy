1e8759844a440ca94587658bab97533c5d9eddae
Kristof Vandam <kristof.vandam@gmail.com>
2018-12-26 13:45:46 +0100

    optional private devices and limit processes

--- manifests/init.pp
+++ manifests/init.pp
@@ -28,20 +28,22 @@
 #
 class caddy (
 
-  String                         $install_path     = $caddy::params::install_path,
-  String                         $caddy_user       = $caddy::params::caddy_user,
-  String                         $caddy_group      = $caddy::params::caddy_group,
-  String                         $caddy_log_dir    = $caddy::params::caddy_log_dir,
-  String                         $caddy_tmp_dir    = $caddy::params::caddy_tmp_dir,
-  String                         $caddy_ssl_dir    = $caddy::params::caddy_ssl_dir,
-  String                         $caddy_home       = $caddy::params::caddy_home,
-  Enum['personal', 'commercial'] $caddy_license    = 'personal',
-  Enum['on','off']               $caddy_telemetry  = 'off',
-  String                         $caddy_features   = 'http.filter,http.git,http.ipfilter',
-  Stdlib::Port                   $caddy_http_port  = $caddy::params::caddy_http_port,
-  Stdlib::Port                   $caddy_https_port = $caddy::params::caddy_https_port,
-  Optional[String[1]]            $caddy_account_id,
-  Optional[String[1]]            $caddy_api_key,
+  String                         $install_path          = $caddy::params::install_path,
+  String                         $caddy_user            = $caddy::params::caddy_user,
+  String                         $caddy_group           = $caddy::params::caddy_group,
+  String                         $caddy_log_dir         = $caddy::params::caddy_log_dir,
+  String                         $caddy_tmp_dir         = $caddy::params::caddy_tmp_dir,
+  String                         $caddy_ssl_dir         = $caddy::params::caddy_ssl_dir,
+  String                         $caddy_home            = $caddy::params::caddy_home,
+  Enum['personal', 'commercial'] $caddy_license         = 'personal',
+  Optional[String[1]]            $caddy_account_id      = undef,
+  Optional[String[1]]            $caddy_api_key         = undef,
+  Enum['on','off']               $caddy_telemetry       = 'off',
+  String                         $caddy_features        = 'http.filter,http.git,http.ipfilter',
+  Stdlib::Port                   $caddy_http_port       = $caddy::params::caddy_http_port,
+  Stdlib::Port                   $caddy_https_port      = $caddy::params::caddy_https_port,
+  Boolean                        $caddy_private_devices = $caddy::params::caddy_private_devices,
+  Integer                        $caddy_limit_processes = $caddy::params::caddy_limit_processes,
 
   )inherits caddy::params{
 
--- manifests/params.pp
+++ manifests/params.pp
@@ -15,15 +15,18 @@ class caddy::params {
 
   case $::osfamily {
     'RedHat':  {
-      $install_path     = '/usr/local/bin'
-      $caddy_home       = '/etc/ssl/caddy'
-      $caddy_user       = 'caddy'
-      $caddy_group      = 'caddy'
-      $caddy_log_dir    = '/var/log/caddy'
-      $caddy_tmp_dir    = '/tmp'
-      $caddy_ssl_dir    = "${caddy_home}/.caddy"
-      $caddy_http_port  = 80
-      $caddy_https_port = 443
+      $install_path          = '/usr/local/bin'
+      $caddy_home            = '/etc/ssl/caddy'
+      $caddy_user            = 'caddy'
+      $caddy_group           = 'caddy'
+      $caddy_log_dir         = '/var/log/caddy'
+      $caddy_tmp_dir         = '/tmp'
+      $caddy_ssl_dir         = "${caddy_home}/.caddy"
+      $caddy_http_port       = 80
+      $caddy_https_port      = 443
+      $caddy_private_devices = true,
+      $caddy_limit_processes = 64,
+
     }
 
     default:  {
