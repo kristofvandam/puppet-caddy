09e32a219461acbd2786baf4cd17a39503133d3b
Kristof Vandam <kristof.vandam@gmail.com>
2018-12-28 07:38:17 +0100

    fix travis

--- manifests/params.pp
+++ manifests/params.pp
@@ -19,6 +19,11 @@ class caddy::params {
       $caddy_home            = '/etc/ssl/caddy'
       $caddy_user            = 'caddy'
       $caddy_group           = 'caddy'
+      $caddy_license         = 'personal'
+      $caddy_account_id      = undef
+      $caddy_api_key         = undef
+      $caddy_telemetry       = 'off'
+      $caddy_features        = 'http.filter,http.git,http.ipfilter'
       $caddy_log_dir         = '/var/log/caddy'
       $caddy_tmp_dir         = '/tmp'
       $caddy_ssl_dir         = "${caddy_home}/.caddy"
@@ -26,7 +31,6 @@ class caddy::params {
       $caddy_https_port      = 443
       $caddy_private_devices = true
       $caddy_limit_processes = 64
-
     }
 
     default:  {
